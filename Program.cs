28b6fc78150a4df02c473154f51ec373b8fd0e18
Sadman <32922365+sadman6259@users.noreply.github.com>
2021-11-10 11:40:11 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,27 @@
-﻿bool peakhour = false;
 DateTime starttime = new DateTime(2019, 08, 31, 08, 59, 13);
 DateTime endtime = new DateTime(2019, 08, 31, 09, 00, 39);
 DateTime PeakHourStarttime = new DateTime(2019, 08, 31, 09, 00, 00);
 DateTime PeakHourEndtime = new DateTime(2019, 08, 31, 23, 00, 00);
 
-var diffInSeconds = (endtime - starttime).TotalSeconds;
 double TotalCost = 0;
 
+while (starttime <= endtime)
 
-double startTimeInseconds = starttime.Subtract(DateTime.MinValue).TotalSeconds;
-double endTimeInseconds = endtime.Subtract(DateTime.MinValue).TotalSeconds;
-double PeakHourStarttimeInseconds = PeakHourStarttime.Subtract(DateTime.MinValue).TotalSeconds;
-double PeakHourEndtimeInseconds = PeakHourEndtime.Subtract(DateTime.MinValue).TotalSeconds;
-
-
-
-while (startTimeInseconds <= endTimeInseconds)
 {
-    startTimeInseconds += 20;
-    if(startTimeInseconds >= PeakHourStarttimeInseconds && startTimeInseconds  <= PeakHourEndtimeInseconds)
+
+    if ((starttime <= PeakHourStarttime) && (starttime <= endtime) && (starttime.AddSeconds(20) < PeakHourStarttime))
+    {
+        TotalCost += 20;
+
+    }
+    else
     {
         TotalCost += 30;
+
     }
-    TotalCost += 20;
+    starttime = starttime.AddSeconds(20);
+
+
 
 }
 
